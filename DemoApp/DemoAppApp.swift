512179e07e2942a19cf45c2e36cb45323701668c
vbugrym <vbugrym@lohika.com>
2023-10-25 15:36:22 +0300

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by vbugrym on 25.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
